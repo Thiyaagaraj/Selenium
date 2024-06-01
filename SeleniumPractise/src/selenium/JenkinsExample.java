e30742881da511449614c864c65bd11523993484
ThiyagSelenium <165637418+ThiyagSelenium@users.noreply.github.com>
2024-06-01 12:23:38 +0530

    Added Jenkins First program

---
+++ SeleniumPractise/src/selenium/JenkinsExample.java
@@ -0,0 +1,11 @@
+package selenium;
+
+public class JenkinsExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Jenkins Program");
+		System.out.println("This is Thiyagaraj Logasundaram");
+
+	}
+}
