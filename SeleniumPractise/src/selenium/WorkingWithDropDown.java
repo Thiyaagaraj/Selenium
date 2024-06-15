0185eb3525f8cb89b0bbafd37237eb871d38988d
ThiyagSelenium <165637418+ThiyagSelenium@users.noreply.github.com>
2024-06-15 16:06:33 +0530

    Dropdown

--- SeleniumPractise/src/selenium/WorkingWithDropDown.java
+++ SeleniumPractise/src/selenium/WorkingWithDropDown.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class WorkingWithDropDown {
 
@@ -18,24 +17,25 @@ public class WorkingWithDropDown {
 		driver.get("https://www.leafground.com/select.xhtml");
 
 		
-		 WebElement dropdown1 = driver.findElement(By.xpath(
-		 "//*[@id=\"j_idt87\"]/div/div[1]/div[1]/div/div/select"));
-		 
-		 Select select = new Select(dropdown1); 
-		 select.selectByIndex(4);
-		
-		 List<WebElement> options = select.getOptions();
-		 int size = options.size();
-		 System.out.println("Size of the dropdown :"+size);
-		
 		/*
-		 * WebElement dropdown =
-		 * driver.findElement(By.xpath("//*[@id=\"j_idt87:country_input\"]")); Select
-		 * select = new Select(dropdown); select.selectByValue("India");
-		 * Thread.sleep(3000); select.selectByVisibleText("Brazil");
+		 * WebElement dropdown1 = driver.findElement(By.xpath(
+		 * "//*[@id=\"j_idt87\"]/div/div[1]/div[1]/div/div/select"));
+		 * 
+		 * Select select = new Select(dropdown1); select.selectByIndex(4);
+		 * 
+		 * 
+		 * List<WebElement> options = select.getOptions(); int size = options.size();
+		 * System.out.println("Size of the dropdown :"+size);
 		 */
-		
-		
+		driver.findElement(By.id("j_idt87:country_label")).click();
+		List<WebElement> dropdown2 = driver.findElements(By.xpath("//*[@id='j_idt87:country_items']/li"));
+
+		//WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"j_idt87:country_input\"]"));
+		for (WebElement webElement : dropdown2) {
+			if(webElement.getText().equalsIgnoreCase("Brazil")){
+				webElement.click();
+			}
+		}
 	}
 
 }
